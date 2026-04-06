3150e90f5ca3c22fd2a54a99b2d53abecb3d3606
Shivane Kapoor <118840715+shivane1@users.noreply.github.com>
2026-04-06 10:05:15 +0530

    Add files via upload

---
+++ SpringBootThymleaf_CRUD_H2BC/src/main/java/com/test/SpringBootThymleafCrudH2BcApplication.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThymleafCrudH2BcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootThymleafCrudH2BcApplication.class, args);
+	}
+
+}
---
+++ SpringBootThymleaf_CRUD_H2BC/src/test/java/com/test/SpringBootThymleafCrudH2BcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootThymleafCrudH2BcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
